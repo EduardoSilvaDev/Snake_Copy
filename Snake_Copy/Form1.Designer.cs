2a2d107582d449c6efd93649a7ef5c843cb28742
EduardoSilvaDev <carlos_duhduh5569@hotmail.com>
2022-06-19 02:29:27 -0300

    commit initial

---
+++ Snake_Copy/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Snake_Copy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pn_tela = new System.Windows.Forms.Panel();
+            this.lb_pontos = new System.Windows.Forms.Label();
+            this.menu_top = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iniciarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.t_Frame = new System.Windows.Forms.Timer(this.components);
+            this.menu_top.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pn_tela
+            // 
+            this.pn_tela.Location = new System.Drawing.Point(12, 58);
+            this.pn_tela.Name = "pn_tela";
+            this.pn_tela.Size = new System.Drawing.Size(428, 428);
+            this.pn_tela.TabIndex = 0;
+            // 
+            // lb_pontos
+            // 
+            this.lb_pontos.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_pontos.Location = new System.Drawing.Point(12, 31);
+            this.lb_pontos.Name = "lb_pontos";
+            this.lb_pontos.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lb_pontos.Size = new System.Drawing.Size(425, 23);
+            this.lb_pontos.TabIndex = 1;
+            this.lb_pontos.Text = "PONTOS: 0";
+            // 
+            // menu_top
+            // 
+            this.menu_top.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menu_top.Location = new System.Drawing.Point(0, 0);
+            this.menu_top.Name = "menu_top";
+            this.menu_top.Size = new System.Drawing.Size(449, 24);
+            this.menu_top.TabIndex = 2;
+            this.menu_top.Text = "Menu";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iniciarToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // iniciarToolStripMenuItem
+            // 
+            this.iniciarToolStripMenuItem.Name = "iniciarToolStripMenuItem";
+            this.iniciarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.iniciarToolStripMenuItem.Text = "Iniciar";
+            this.iniciarToolStripMenuItem.Click += new System.EventHandler(this.iniciarToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // t_Frame
+            // 
+            this.t_Frame.Interval = 50;
+            this.t_Frame.Tick += new System.EventHandler(this.t_Frame_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 498);
+            this.Controls.Add(this.lb_pontos);
+            this.Controls.Add(this.pn_tela);
+            this.Controls.Add(this.menu_top);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menu_top;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Clicado);
+            this.menu_top.ResumeLayout(false);
+            this.menu_top.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pn_tela;
+        private System.Windows.Forms.Label lb_pontos;
+        private System.Windows.Forms.MenuStrip menu_top;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iniciarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.Timer t_Frame;
+    }
+}
+
---
+++ Snake_Copy/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake_Copy
+{
+    public partial class Form1 : Form
+    {
+        Game Game;
+        public Form1()
+        {
+            InitializeComponent();
+            Game = new Game(ref t_Frame,ref lb_pontos, ref pn_tela);
+        }
+
+        private void iniciarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Game.StarTGame();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void t_Frame_Tick(object sender, EventArgs e)
+        {
+            Game.Tick();
+        }
+
+        private void Clicado(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left || e.KeyCode == Keys.Right || e.KeyCode == Keys.Up || e.KeyCode == Keys.Down)
+            {
+                Game.Arrow = e.KeyCode;
+            }
+        }
+    }
+}
